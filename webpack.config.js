f9c506b30ae22423323f887c0e9ca32df8347240
saurabh-prosoft <saurabhbhagat98die@gmail.com>
2021-11-22 00:00:31 +0530

    chore

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     } */
   },
   output: {
-    path: path.resolve(__dirname, '_bundles'),
+    path: path.resolve(__dirname, 'bundles'),
     filename: '[name].js',
     libraryTarget: 'umd',
     umdNamedDefine: true
